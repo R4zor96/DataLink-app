5dca2c8ca51f740d942f893eccfa78866bedcbec
R4zor96 <jose.miguel.7897763@gmail.com>
2025-10-09 11:05:32 -0600

    correccion de colores y ormato

--- src/app/features/dashboard/pages/dashboard-login/dashboard-login.component.ts
+++ src/app/features/dashboard/pages/dashboard-login/dashboard-login.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { DashboardRoutingModule } from "../../dashboard-routing.module";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard-login',
-  imports: [],
+  imports: [DashboardRoutingModule, RouterLink],
   templateUrl: './dashboard-login.component.html',
   styleUrl: './dashboard-login.component.css'
 })
