ee657136bfe35c46bca880bcbaeb1532f9338d2e
R4zor96 <jose.miguel.7897763@gmail.com>
2025-10-23 12:33:33 -0600

    resegin V1

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,22 @@ module.exports = {
       colors: {
         'brand-red': '#E71D36',
         'brand-red-dark': '#C2172D',
-        'brand-dark': '#1E293B', // Un azul/gris oscuro para fondos y texto
+        'brand-dark': '#1E293B',
+        // Paleta Material (ejemplos)
+        'primary': { // Azul principal
+          light: '#64b5f6',
+          DEFAULT: '#2196f3',
+          dark: '#1976d2',
+        },
+        'secondary': { // Gris para texto secundario, bordes
+           light: '#e0e0e0',
+           DEFAULT: '#9e9e9e',
+           dark: '#616161',
+        },
+        'background': { // Fondos
+           DEFAULT: '#f5f5f5', // Gris claro para el fondo general
+           paper: '#ffffff',   // Blanco para tarjetas
+        }
       }
     },
   },
